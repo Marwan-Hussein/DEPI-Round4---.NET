8c328c644fb5683776230e5731c1ab8458ce5fd9
marwan hussein <marawanlabana649@gmail.com>
2026-02-25 04:09:10 +0200

    pb5

--- Day10/Day10/Program.cs
+++ Day10/Day10/Program.cs
@@ -9,11 +9,11 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            /*#region pb1
+            #region pb1
             problem(1);
             Employee[] emps =
             {
-                new Employee("Maro", (int)4.5e4),
+                new Employee("Marwanello", (int)4.5e4),
                 new Employee("ahmed", 12500),
                 new Employee("john", (int)2.5e3)
             };
@@ -21,6 +21,7 @@ namespace Day10
             Sort(emps, del);
             Print(emps); 
             #endregion
+            /*
             #region pb2
             problem(2);
             int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
@@ -37,7 +38,6 @@ namespace Day10
             Print(arr3); // hi i'm maro
             #endregion
 
-            */
             #region pb4
             problem(4);
             Manager[] mngers =
@@ -49,10 +49,13 @@ namespace Day10
             Manager.Sort(mngers);
             Print(mngers);
             #endregion
+            */
 
             #region pb5
             problem(5);
-
+            Func<Employee, Employee, bool> lessLen = (emp1, emp2) => emp1.Name.Length < emp2.Name.Length;
+            SortingTwo(emps, lessLen);
+            Print(emps);
             #endregion
 
             #region pb6
