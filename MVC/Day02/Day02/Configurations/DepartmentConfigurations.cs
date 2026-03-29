e3db84682095a7324c4f4bfdb4321148f28381fb
marwan hussein <marawanlabana649@gmail.com>
2026-03-29 16:49:54 +0200

    DepartmentConfigurations

--- MVC/Day02/Day02/Configurations/DepartmentConfigurations.cs
+++ MVC/Day02/Day02/Configurations/DepartmentConfigurations.cs
@@ -1,6 +1,14 @@
-﻿namespace Day02.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Day02.Models;
+
+namespace Day02.Configurations
 {
     public class DepartmentConfigurations
     {
+        public void Configure(EntityTypeBuilder<Department> Dept)
+        {
+            Dept.HasKey(d => d.Id);
+        }
     }
 }
