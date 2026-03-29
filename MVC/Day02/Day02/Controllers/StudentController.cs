402887863d7f053d47fcd5b57ee21cabdbb6b8fb
marwan hussein <marawanlabana649@gmail.com>
2026-03-30 01:05:50 +0200

    completing Views

--- MVC/Day02/Day02/Controllers/StudentController.cs
+++ MVC/Day02/Day02/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Day02.Bussiness_Logic;
+using Day02.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Day02.Controllers
 {
@@ -7,7 +9,15 @@ namespace Day02.Controllers
         // Student/Details?id=5
         public IActionResult Details(int id)
         {
-            return View();
+            IGetable<Student> getable = new StudentBL();
+            return View("Details", getable.GetById(id));
+        }
+
+        // Student/ShowAll
+        public IActionResult ShowAll()
+        {
+            IGetable<Student> getable = new StudentBL();
+            return View("ShowAll", getable.GetAll());
         }
     }
 }
