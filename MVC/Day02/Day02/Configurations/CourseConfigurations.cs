d050040025598cb598e5cc08f1db99fee11b137d
marwan hussein <marawanlabana649@gmail.com>
2026-03-29 16:48:54 +0200

    CourseConfigurations

--- MVC/Day02/Day02/Configurations/CourseConfigurations.cs
+++ MVC/Day02/Day02/Configurations/CourseConfigurations.cs
@@ -1,6 +1,18 @@
-﻿namespace Day02.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Day02.Models;
+
+namespace Day02.Configurations
 {
     public class CourseConfigurations
     {
+        public void Configure(EntityTypeBuilder<Course> Crs)
+        {
+            Crs.HasKey(c => c.Id);
+            Crs.HasOne(C => C.Department)
+                .WithMany(D => D.Courses)
+                .HasForeignKey(C => C.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
