37a005c326ad8cb5191b79ff40afddd81e406910
marwan hussein <marawanlabana649@gmail.com>
2026-03-29 17:20:37 +0200

    CollegeSystemDbContext

---
+++ MVC/Day02/Day02/DbContext/CollegeSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection;
+using Day02.Models;
+using Day02.Configurations;
+namespace Day02.DbContext
+{
+    public class CollegeSystemDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = .; Database = CollegeSystem; Trusted_Connection = True;"
+                , b => b.MigrationsAssembly("EF02")
+                );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
