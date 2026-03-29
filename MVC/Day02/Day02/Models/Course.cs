8a88f02462850fdebe75c9327b92feab0bf5fbd1
marwan hussein <marawanlabana649@gmail.com>
2026-03-29 16:31:00 +0200

    declare Course Model

--- MVC/Day02/Day02/Models/Course.cs
+++ MVC/Day02/Day02/Models/Course.cs
@@ -1,7 +1,24 @@
-﻿namespace Day02.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day02.Models
 {
     public class Course
     {
-        
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Degree { get; set; }
+        public double MinDegree { get; set; }
+
+        // FK
+        public int DepartmentId { get; set; }
+
+        // NPs
+        public Department Department { get; set; }
+        public List<Teacher> Teachers { get; set; }
+        public List<StuCrsRes> StuCrsResult { get; set; }
     }
 }
