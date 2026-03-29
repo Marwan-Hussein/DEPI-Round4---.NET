1b7a1db233d898230d839b0a088ca09ba0b93741
marwan hussein <marawanlabana649@gmail.com>
2026-03-29 16:58:02 +0200

    TeacherConfigurations

--- MVC/Day02/Day02/Configurations/TeacherConfigurations.cs
+++ MVC/Day02/Day02/Configurations/TeacherConfigurations.cs
@@ -1,6 +1,26 @@
-﻿namespace Day02.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Day02.Models;
+
+namespace Day02.Configurations
 {
     public class TeacherConfigurations
     {
+        public void Configure(EntityTypeBuilder<Teacher> Teacher)
+        {
+            Teacher.HasKey(T => T.Id);
+            Teacher.Property(T => T.Salary)
+                .HasPrecision(6, 2);
+
+            Teacher.HasOne(T => T.Department)
+                .WithMany(D => D.Teachers)
+                .HasForeignKey(T => T.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            Teacher.HasOne(T => T.Course)
+                .WithMany(C => C.Teachers)
+                .HasForeignKey(T => T.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
