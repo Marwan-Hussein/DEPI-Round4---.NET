f811b8fda95a1f314ba3c8a8dff5773025a836eb
marwan hussein <marawanlabana649@gmail.com>
2026-03-29 16:53:05 +0200

    StuCrsResConfigurations

--- MVC/Day02/Day02/Configurations/StuCrsResConfigurations.cs
+++ MVC/Day02/Day02/Configurations/StuCrsResConfigurations.cs
@@ -1,6 +1,24 @@
-﻿namespace Day02.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Day02.Models;
+
+namespace Day02.Configurations
 {
     public class StuCrsResConfigurations
     {
+        public void Configure(EntityTypeBuilder<StuCrsRes> SCR)
+        {
+            SCR.HasKey(x => new { x.StudentId, x.CourseId });
+
+            SCR.HasOne(SCR => SCR.Student)
+                .WithMany(S => S.StuCrsResult)
+                .HasForeignKey(SCR => SCR.StudentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            SCR.HasOne(SCR => SCR.Course)
+                .WithMany(C => C.StuCrsResult)
+                .HasForeignKey(SCR => SCR.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
