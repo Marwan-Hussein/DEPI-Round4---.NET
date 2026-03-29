142ebd060cc9387a8247820f00692b4af306617d
marwan hussein <marawanlabana649@gmail.com>
2026-03-30 00:22:06 +0200

    adding IGetable interface

---
+++ MVC/Day02/Day02/Bussiness Logic/IGetable.cs
@@ -0,0 +1,11 @@
+﻿using Day02.Data.DbContexts;
+
+namespace Day02.Bussiness_Logic
+{
+    public interface IGetable<T>
+    {
+        CollegeSystemDbContext Context { set; get; }
+        public List<T> GetAll();
+        public T GetById(int id);
+    }
+}
---
+++ MVC/Day02/Day02/Controllers/StudentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day02.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Student/Details?id=5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
