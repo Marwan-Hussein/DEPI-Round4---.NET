f01ca3977e9cc5ed354843fc4f0f1b1a62764700
marwan hussein <marawanlabana649@gmail.com>
2026-03-29 16:19:22 +0200

    building department model

--- MVC/Day02/Day02/Models/Department.cs
+++ MVC/Day02/Day02/Models/Department.cs
@@ -1,6 +1,19 @@
-﻿namespace Day02.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day02.Models
 {
     public class Department
     {
+        public int Id { get; set; }
+
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+
+        public string? MgrName { get; set; }
+
+        // Navigation properties (NPs)
+        public List<Teacher> Teachers { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Student> Students { get; set; }
     }
 }
