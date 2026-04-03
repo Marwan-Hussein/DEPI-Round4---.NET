d67b9a4ad458fbe9e288267c6cfc6d5bdfa8f3fc
marwan hussein <marawanlabana649@gmail.com>
2026-04-03 03:09:03 +0200

    adding Student => Controller + button in index

--- MVC/Day04/Day03/Controllers/StudentController.cs
+++ MVC/Day04/Day03/Controllers/StudentController.cs
@@ -6,6 +6,7 @@ namespace Day04.Controllers
 {
     public class StudentController : Controller
     {
+        StudentBL studentBL = new();
         // Student/Index
         public IActionResult Index() // => default action
         {
@@ -19,6 +20,22 @@ namespace Day04.Controllers
             IGetable<Student> getable = new StudentBL();
             var student = getable.GetById(id);
             return View("ShowDetails", student);  // student may be null; view handles it
+        }
+
+        [HttpGet]
+        public IActionResult Add() => View("Add");
+
+        [HttpPost]
+        public IActionResult SaveAdd(Student std)
+        {
+            if(std.Name == null || std.Age <= 0 || std.DepartmentId <= 1)
+            {
+                ViewBag.Error = "Invalid input. Please fill all fields correctly.";
+                return View("Add");
+            }
+
+            studentBL.Add(std);
+            return RedirectToAction(nameof(Index));
         }
 
     }
