5bce02ae46f40d12af7102ff2b2e5de81626baee
marwan hussein <marawanlabana649@gmail.com>
2026-04-11 23:32:25 +0200

    adding the StuDepVM again

---
+++ MVC/Day04/Day04/ViewModels/StudentDep.cs
@@ -0,0 +1,15 @@
+﻿using Day04.Models;
+
+namespace Day04.ViewModels
+{
+    public class StudentDep
+    {
+        // Student
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Department Department { get; set; }
+
+        // Department
+        public List<Department> Departments { get; set; }
+    }
+}
