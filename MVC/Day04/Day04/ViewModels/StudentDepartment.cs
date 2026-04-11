fa08a3981da1442b1e9116ef6e49e61ba4d9ba92
marwan hussein <marawanlabana649@gmail.com>
2026-04-11 23:28:21 +0200

    update add Student to choose from a list

---
+++ MVC/Day04/Day04/ViewModels/StudentDepartment.cs
@@ -0,0 +1,13 @@
+﻿using Day04.Models;
+namespace Day04.ViewModels
+{
+    public class StudentDepartment
+    {
+        // Student
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Department Department { get; set; }
+
+    }
+}
